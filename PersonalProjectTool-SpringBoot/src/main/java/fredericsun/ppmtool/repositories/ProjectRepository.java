f250961166d5b4f4fe3feff8322977895d813e22
Yue Sun <ysun276@wisc.edu>
2019-12-29 23:19:30 -0500

    server built-up

---
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package fredericsun.ppmtool.repositories;
+
+import fredericsun.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
