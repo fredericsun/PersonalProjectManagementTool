2a9ae63d05c62e6ec90558787bf1712158ea9b95
Yue Sun <ysun276@wisc.edu>
2019-12-29 23:55:50 -0500

    set up project object validation

--- PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/web/ProjectController.java
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/web/ProjectController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,8 +25,16 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors())
-            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+        if (result.hasErrors()) {
+            HashMap<String, String> errorMsg = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMsg.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<HashMap<String, String>>(errorMsg, HttpStatus.BAD_REQUEST);
+        }
+
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
