654b5610a7e1b950b5fd0fa4c19190355422ca4d
Yue Sun <ysun276@wisc.edu>
2019-12-30 00:55:50 -0500

    custom unique id exception

---
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/exceptions/ProjectIDExceptionResponse.java
@@ -0,0 +1,17 @@
+package fredericsun.ppmtool.exceptions;
+
+public class ProjectIDExceptionResponse {
+    private String projectIdentifier;
+
+    public ProjectIDExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
--- PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package fredericsun.ppmtool.services;
 
 import fredericsun.ppmtool.domain.Project;
+import fredericsun.ppmtool.exceptions.ProjectIdException;
 import fredericsun.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        // Logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
 }
