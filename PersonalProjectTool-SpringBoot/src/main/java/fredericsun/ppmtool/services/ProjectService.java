df1110a92a4e501d7a39aad8bb2a7b729bed79c3
Yue Sun <ysun276@wisc.edu>
2019-12-30 23:08:59 -0500

    find all project

--- PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
@@ -17,14 +17,18 @@ public class ProjectService {
             return projectRepository.save(project);
         }
         catch (Exception e) {
-            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
     }
 
     public Project findProjectByIdentifier(String projectId) {
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if (project == null)
-            throw new ProjectIdException("Project ID " + projectId + " does not exist");
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
         return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
     }
 }
