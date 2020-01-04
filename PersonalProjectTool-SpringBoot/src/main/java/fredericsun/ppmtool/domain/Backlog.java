7d6ba362d72cc6b33544eb8c7a07e3b9332bd785
Yue Sun <ysun276@wisc.edu>
2020-01-04 13:16:51 -0500

    toggle project and backlog relation

--- PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/domain/Backlog.java
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/domain/Backlog.java
@@ -1,9 +1,8 @@
 package fredericsun.ppmtool.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 public class Backlog {
@@ -14,6 +13,10 @@ public class Backlog {
     private String projectIdentifier;
 
     // One to one log with a project
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
+    private Project project;
 
     // One to many log with project tasks
 
@@ -43,5 +46,13 @@ public class Backlog {
 
     public void setProjectIdentifier(String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
     }
 }
--- PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
+++ PersonalProjectTool-SpringBoot/src/main/java/fredericsun/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package fredericsun.ppmtool.services;
 
+import fredericsun.ppmtool.domain.Backlog;
 import fredericsun.ppmtool.domain.Project;
 import fredericsun.ppmtool.exceptions.ProjectIdException;
+import fredericsun.ppmtool.repositories.BacklogRepository;
 import fredericsun.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,22 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
+    @Autowired
+    private BacklogRepository backlogRepository;
 
     public Project saveOrUpdateProject(Project project) {
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if (project.getId() == null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+            else {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
         }
         catch (Exception e) {
