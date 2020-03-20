8a518cfb9ede7aaa707a72b7b82cb3f98d1522a0
Yue Sun <ysun276@wisc.edu>
2020-03-19 23:22:44 -0400

    Updates

---
+++ precache-manifest.3f7bb88eb87deadd5a363778f81f00d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c52bd1bdef995f93a98fd7c4f088aae",
+    "url": "/myplannerapp/index.html"
+  },
+  {
+    "revision": "2f995da084d5cb4ef1dc",
+    "url": "/myplannerapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "576d3da560941f79ec9c",
+    "url": "/myplannerapp/static/js/2.a5de995a.chunk.js"
+  },
+  {
+    "revision": "1ae01fb0d60dfdefe7f9d6cbefaaa517",
+    "url": "/myplannerapp/static/js/2.a5de995a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f995da084d5cb4ef1dc",
+    "url": "/myplannerapp/static/js/main.7e5cc3e4.chunk.js"
+  },
+  {
+    "revision": "df443b3d12deb3b91241",
+    "url": "/myplannerapp/static/js/runtime-main.48a60161.js"
+  }
+]);
